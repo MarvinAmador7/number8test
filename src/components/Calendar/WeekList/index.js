52b11513e6f6acbf8d23bc22db85c6d0a826fadf
MarvinAmador7 <marvin.ac.cr@gmail.com>
2018-07-18 19:56:02 -0600

    add week list component

---
+++ src/components/Calendar/WeekList/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+// Helpers
+import { getWeekDate } from "../../../helpers/calendar";
+
+// Components
+import Week from "../Week";
+
+const WeekList = ({ month, dateRange }) => {
+  let weeks = [];
+  let weekCounter = 0;
+  let date = getWeekDate(month);
+  let isDone = false;
+  let monthIndex = date.month();
+
+  while (!isDone) {
+    weeks.push(
+      <Week
+        key={date}
+        date={date.clone()}
+        month={month}
+        dateRange={dateRange}
+      />,
+    );
+    date.add(1, "week");
+
+    isDone = weekCounter++ > 2 && monthIndex !== date.month();
+    monthIndex = date.month();
+  }
+
+  return weeks;
+};
+
+export default WeekList;
