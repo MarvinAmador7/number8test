3f9945d04094145ba88e9978473b8bbf0697f201
MarvinAmador7 <marvin.ac.cr@gmail.com>
2018-07-18 19:41:42 -0600

    add theme helper

---
+++ src/helpers/theme.js
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    weekend: "#ffee88",
+    week: "#a7b000 ",
+    disabled: "#bdbdbd",
+  },
+  flex: (items = "center", content = "center", direction = "row") => {
+    return {
+      display: "flex",
+      flexDirection: direction,
+      alignItems: items,
+      justifyContent: content,
+    };
+  },
+  fonts: {
+    light: { "font-family": "Lato, sans-serif", fontWeight: 300 },
+    regular: { "font-family": "Lato, sans-serif", fontWeight: 400 },
+    bold: { "font-family": "Lato, sans-serif", fontWeight: 700 },
+  },
+};
